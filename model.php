670d92d1d2b617d5c6bdd1c6c74cf324da3ebd73
Grant Marshall <grantrmarshall97@gmail.com>
2020-03-15 15:56:27 -0700

    comments

--- model.php
+++ model.php
@@ -23,25 +23,32 @@ function match($name, $tag, $email, $comp, $hours, $timezone, $gender, $game)
 	//if 'Doesn't Matter' is chosen
 	if($hours == 'hoursDm' && $timezone == 'timeDm' && $gender == 'genderDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Game = '$game'";
-	}else if($hours == 'hoursDm' && $timezone == 'timeDm'){
+	}
+	else if($hours == 'hoursDm' && $timezone == 'timeDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Gender = '$gender' AND Game = '$game'";
-	}else if ($timezone == 'timeDm' && $gender == 'genderDm'){
+	}
+	else if ($timezone == 'timeDm' && $gender == 'genderDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Hours = '$hours' AND Game = '$game'";
-	}else if ($hours == 'hoursDm' && $gender == 'genderDm'){
+	}
+	else if ($hours == 'hoursDm' && $gender == 'genderDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Timezone = '$timezone' AND Game = '$game'";
-	}else if($hours == 'hoursDm'){
+	}
+	else if($hours == 'hoursDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Timezone = '$timezone' AND Gender = '$gender' AND Game = '$game'";
-	}else if($timezone == 'timeDm'){
+	}
+	else if($timezone == 'timeDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Hours = '$hours' AND Gender = '$gender' AND Game = '$game'";
-	}else if($gender == 'genderDm'){
+	}
+	else if($gender == 'genderDm'){
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Hours = '$hours' AND Timezone = '$timezone' AND Game = '$game'";
-	}else{
+	}
+	else{
 		$sql = "SELECT Name, Tag, Game FROM gcUsers WHERE Competitive = '$comp' AND Hours = '$hours' AND Timezone = '$timezone' AND Gender = '$gender' AND Game = '$game'";
 	}
-
+	// get resulst of Query built above
     $result = mysqli_query($conn, $sql);
-		if (mysqli_num_rows($result) > 0)
-			while($row = $result->fetch_assoc()) {
+		if (mysqli_num_rows($result) > 0) // if result set of query > 0
+			while($row = $result->fetch_assoc()) { // for each row as an associative array in result put into $row
 				$message = 'You matched with ' . $row["Name"] . '! Discord tag: ' . $row["Tag"] . '. Have fun playing ' . $row["Game"] . '!';
 			}
   	else
